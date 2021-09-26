7413f2cc09b827e453429e06a80cecb19c8cae29
LukaDev <47296785+lukadev-0@users.noreply.github.com>
2021-09-26 14:08:08 +0200

    project setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# lukadev-buffer
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+/**
+ * @public
+ */
+export const getPublic = () => 'Its Public!'
+
+/**
+ * @internal
+ */
+export const getInternal = () => 'Its Internal!'
+
+/**
+ * @beta
+ */
+export const getBeta = () => 'Its Beta!'
+
+/**
+ * @alpha
+ */
+export const getAlpha = () => 'Its Alpha!'
